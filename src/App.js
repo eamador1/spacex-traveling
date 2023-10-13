3849ada0874aaf1e74aeb54857bbcd62c0262164
Erwin Amador <eamador0305@gmail.com>
2023-10-12 19:59:17 -0600

    set and navlink

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import {
+  BrowserRouter, NavLink
+} from 'react-router-dom';
+
+import './App.css';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <header>
+        Hello World
+        <NavLink>
+          
+        </NavLink>
+      </header>
+    </BrowserRouter>
+  );
+}
+
+export default App;
